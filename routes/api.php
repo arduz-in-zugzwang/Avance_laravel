d9b701a0ac446a53e2993f1fb8d34fff27e3f83c
arduz-in-zugzwang <sofiarduzcan@gmail.com>
2026-06-17 22:48:00 -0400

    #miedo parte 3

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,11 @@ use App\Http\Controllers\Api\TagController;
 use App\Http\Controllers\Api\LetraController;
 use App\Http\Controllers\Api\ComentariosArtistaController;
 use App\Http\Controllers\Api\UserController;
-
+use App\Http\Controllers\Api\CancionTagController;
+use App\Http\Controllers\Api\EscuchanController;
+use App\Http\Controllers\Api\LikeController;
+use App\Http\Controllers\Api\PlaylistCancioneController;
+use App\Http\Controllers\Api\SiguenController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -27,3 +31,8 @@ Route::apiResource('tags', TagController::class);
 Route::apiResource('letras', LetraController::class);
 Route::apiResource('comentarios-artista', ComentariosArtistaController::class);
 Route::apiResource('users', UserController::class);
+Route::apiResource('cancion-tag', CancionTagController::class);
+Route::apiResource('escuchans', EscuchanController::class);
+Route::apiResource('likes', LikeController::class);
+Route::apiResource('playlist-canciones', PlaylistCancioneController::class);
+Route::apiResource('siguens', SiguenController::class);
